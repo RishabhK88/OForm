dd5b7aa3061d4d707d379764d8b588d2f5a7b6cc
Rishabh <rishabhrare8@gmail.com>
2021-11-23 19:55:11 +0530

    Update settings.py

--- BOH/settings.py
+++ BOH/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_4=cipu5r^w_fx-1)4f%ba+-(!op6mg^gloz2e*z=v@znkaad-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
